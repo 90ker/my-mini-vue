af50c8f0cf8e0a5d7fd2c1701c92b975380c7cae
zhang <zhang@qq.com>
2023-03-16 16:16:05 +0800

    1.抽出track、trigger 2.创建createReactive隔离环境 3.改变bucket存储结构 4.activeEffect在执行完fn后清除

--- src/reactive.ts
+++ src/reactive.ts
@@ -1,20 +1,54 @@
-let activeEffect; // 将当前执行的effect注册全局effect，方便get里收集
-const bucket = new Set();
-export function reactive(data) {
-    const obj = new Proxy(data, {
-        get(target, key) {
-            activeEffect && bucket.add(activeEffect);
-            return target[key];
-        },
-        set(target, key, newVal) {
-            target[key] = newVal;
-            bucket.forEach(effect => effect());
-            return true;
+export function createReactive() {
+    let activeEffect = null; // 将当前执行的effect注册全局effect，方便get里收集
+    const bucket = new WeakMap();
+
+    function track(target, key) {
+        if (!activeEffect) {
+            return;
         }
-    });
-    return obj;
-}
-export function effect(fn) {
-    activeEffect = fn;
-    fn();
+        let keyEffectMap = bucket.get(target);
+        if (!keyEffectMap) {
+            bucket.set(target, keyEffectMap = new Map());
+        };
+        let effects = keyEffectMap.get(key);
+        if (!effects) {
+            keyEffectMap.set(key, effects = new Set());
+        };
+        effects.add(activeEffect);
+    }
+    function trigger(target, key) {
+        let keyEffectMap = bucket.get(target);
+        if (!keyEffectMap) {
+            return;
+        }
+        let effects = keyEffectMap.get(key);
+        if (!effects) {
+            return;
+        }
+        effects.forEach(effectFn => effectFn());
+    }
+    function reactive(data) {
+        const obj = new Proxy(data, {
+            get(target, key) {
+                track(target, key);
+                return target[key];
+            },
+            set(target, key, newVal) {
+                target[key] = newVal;
+                trigger(target, key);
+                return true;
+            }
+        });
+        return obj;
+    }
+    function effect(fn) {
+        activeEffect = fn;
+        fn();
+        // 每一个effect注册后需要清除，否则会导致互相影响
+        activeEffect = null;
+    }
+    return {
+        reactive,
+        effect
+    }
 }
--- test/reactive.spec.ts
+++ test/reactive.spec.ts
@@ -1,6 +1,7 @@
-import { reactive, effect } from "../src/reactive";
+import { createReactive } from "../src/reactive";
 
 describe('reactive 测试', () => {
+    const { reactive, effect } = createReactive();
     const data = { text: 'hello world' };
     const obj = reactive(data);
     let res;
@@ -10,10 +11,49 @@ describe('reactive 测试', () => {
 
     test('初始化时，effect会被执行一次', () => {
         expect(res).toBe('hello world');
-    })
+    });
 
     test('修改属性，effect触发响应', () => {
         obj.text = 'the vue'
         expect(res).toBe('the vue');
-    })
+    });
+})
+
+describe('reactive 测试2', () => {
+    const { reactive, effect } = createReactive();
+    const data = { num: 1 };
+    const data2 = { num: 1 };
+    const obj = reactive(data);
+    const obj2 = reactive(data2);
+    let count = 0;
+    let count2 = 0;
+
+    effect(() => {
+        count++;
+        obj.num;
+    });
+
+    effect(() => {
+        count2++;
+        obj2.num;
+    });
+
+    test(`为避免property之间互相影响
+        1.将bucket改为weakMap -> Map -> Set结构
+        2.activeEffect完成注册后清除
+    `, () => {
+        expect(count).toBe(1);
+        expect(count2).toBe(1);
+        obj.num = 3;
+        expect(count).toBe(2);
+        expect(count2).toBe(1);
+
+        obj.notExist = 3;
+        expect(count).toBe(2);
+        expect(count2).toBe(1);
+
+        obj2.num = 3;
+        expect(count).toBe(2);
+        expect(count2).toBe(2);
+    });
 })
