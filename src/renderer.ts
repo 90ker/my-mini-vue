9eda8b019ddb3b2c969a30167a6d9bb295d77b17
90ker <z1582373436@gmail.com>
2023-04-04 21:13:53 +0800

    1. 自定义renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,61 @@
-export function renderer(domString, container) {
-    container.innerHTML = domString;
+export const domAPI = {
+    createElement(tag) {
+        return document.createElement(tag);
+    },
+    setElementText(el, text) {
+        el.textContent = text;
+    },
+    setInnerHTML(el, domString) {
+        el.innerHTML = domString;
+    },
+    insert(el, parent, anchor = null) {
+        parent.insertBefore(el, anchor);
+    }
+}
+
+
+
+export function createRenderer(domAPI) {
+    const {
+        createElement,
+        setElementText,
+        insert,
+        setInnerHTML
+    } = domAPI;
+    function render(vNode, container) {
+        if (vNode) {
+            // 创建与更新
+            patch(container._vNode, vNode, container);
+        } else {
+            // 卸载
+            if (container._vNode) {
+                container.innerHTML = '';
+            }
+        }
+        container._vNode = vNode;
+    }
+
+    function patch(n1, n2, container) {
+        if (!n1) {
+            mountElement(n2, container);
+        } else {
+            // 暂时一样
+            mountElement(n2, container);
+        }
+    }
+
+    function mountElement(vNode, container) {
+        if (typeof vNode === 'string') {
+            setInnerHTML(container, vNode);
+        }
+        const el = createElement(vNode.type);
+        if (typeof vNode.children === 'string') {
+            setElementText(el, vNode.children);
+        }
+        insert(el, container);
+    }
+
+    return {
+        render
+    }
 }
--- test/renderer.spec.ts
+++ test/renderer.spec.ts
@@ -1,24 +1,43 @@
 /**
  * @jest-environment jsdom
  */
-import { effect, ref } from '@vue/reactivity';
-import { renderer } from '../src/renderer';
+import { effect, ref, reactive } from '@vue/reactivity';
+import { createRenderer, domAPI } from '../src/renderer';
 import $ from 'jquery';
 
-// const { effect, ref } = VueReactivity;
-describe('renderer 1', () => {
+test('1. 渲染与响应结合', () => {
     document.body.innerHTML = `
         <div id='app'></div>
     `
     const count = ref(1);
+    const renderer = createRenderer(domAPI);
 
     effect(() => {
-        renderer(`<h1>${count.value}</h1>`, $('#app')[0]);
+        renderer.render(`<h1>${count.value}</h1>`, $('#app')[0]);
+    });
+    expect($('#app').text()).toBe('1');
+    count.value++;
+    expect($('#app').text()).toBe('2');
+});
+
+
+test('2. 自定义渲染器', () => {
+    document.body.innerHTML = `
+        <div id='app'></div>
+    `
+    const data = reactive({
+        vNode: {
+            type: 'h1',
+            children: 'hello'
+        }
+    });
+    const renderer = createRenderer(domAPI);
+
+    effect(() => {
+        renderer.render(data.vNode, $('#app')[0]);
     });
 
-    test('渲染与响应结合', () => {
-        expect($('#app').text()).toBe('1');
-        count.value++;
-        expect($('#app').text()).toBe('2');
-    });
+    expect($('#app').text()).toBe('hello');
+    data.vNode = null;
+    expect($('#app').text()).toBe('');
 });
